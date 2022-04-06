a6dd6c7b60fb30b8f5ddd7462fd1499727883747
Nick Englezakis <95222208+nikoengl27@users.noreply.github.com>
2022-04-06 11:11:28 +0100

    back end updated and working

---
+++ client/src/containers/InventoryContainer.js
@@ -0,0 +1,14 @@
+useEffect(() => {
+    let fetches = createFetches()
+    Promise.all(fetches)
+    .then(response => response.json())
+    .then(data => setInVENTORY(data))
+  }, [])
+  
+  const createFetches = () => {
+    let fetches = [];
+    fetches.add(fetch('http://localhost:8080/api/tvs'));
+    fetches.add(fetch('http://localhost:8080/api/monitors'));
+    fetches.add(fetch('http://localhost:8080/api/laptops'));
+    return fetches;
+  
--- client/src/containers/TvContainer.js
+++ client/src/containers/TvContainer.js
@@ -17,6 +17,7 @@ const TvContainer = () => {
         .then(data => setTvs(data))
     }
 
+
     return (
         <Router>
         <Fragment>
